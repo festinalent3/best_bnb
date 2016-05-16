471b61d5a6c703670be9c5d18149359bc3024284
hnamitha1 <namitha.h@gmail.com>
2016-05-16 17:19:34 +0100

    small fix

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,7 +3,7 @@ require 'dm-postgres-adapter'
 require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || 
-  "postgres://namitha:root@localhost/beast_#{ENV['RACK_ENV']}")
+  "postgres://localhost/beast_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 

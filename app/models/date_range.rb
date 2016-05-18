3a3a3c306242899e78b30ce420b6d8612f909b95
Max Provin <maxprovin@outlook.com>
2016-05-18 15:23:51 +0100

    user dates required

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -7,6 +7,7 @@ class DateRange
   end
 
   def generate_range(from_date, to_date)
+    return false if (from_date == '' || to_date == '')
     return [*Date.parse(from_date)..Date.parse(to_date)]
   end
 

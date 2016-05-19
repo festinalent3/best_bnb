94d92694cbbb3a045dbb04b98275e0a898e3feec
Nick <nm.richards@yahoo.com>
2016-05-19 08:20:05 -0700

    Testing for approval button

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,6 +7,7 @@ class Space
   property :description, String, :length => 500
   property :available_dates, Object
   property :requested_dates, Object
+  property :booked_dates, Object
   property :price, Integer
 
   belongs_to :user
